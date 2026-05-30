b75aba6c2f1ab43c8facb5567ca2c71fb00e13d4
Ricardo Díaz <ric.diazs@duocuc.cl>
2026-05-30 06:53:34 -0400

    fix: corrige problemas con lombok en ventas

--- ventas/Springboot-API-REST/src/main/java/com/citt/persistence/entity/Venta.java
+++ ventas/Springboot-API-REST/src/main/java/com/citt/persistence/entity/Venta.java
@@ -32,4 +32,66 @@ public class Venta {
     private LocalDate fechaCompra;
     @NotNull(message = "El campo de despacho debe ser proporcionado")
     private Boolean despachoGenerado = false;
+
+    public Venta() {
+    }
+
+    public Venta(Long idVenta, String direccionCompra, int valorCompra, LocalDate fechaCompra, Boolean despachoGenerado) {
+        this.idVenta = idVenta;
+        this.direccionCompra = direccionCompra;
+        this.valorCompra = valorCompra;
+        this.fechaCompra = fechaCompra;
+        this.despachoGenerado = despachoGenerado;
+    }
+
+    public Long getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Long idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public String getDireccionCompra() {
+        return direccionCompra;
+    }
+
+    public void setDireccionCompra(String direccionCompra) {
+        this.direccionCompra = direccionCompra;
+    }
+
+    public int getValorCompra() {
+        return valorCompra;
+    }
+
+    public void setValorCompra(int valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+
+    public LocalDate getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(LocalDate fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public Boolean getDespachoGenerado() {
+        return despachoGenerado;
+    }
+
+    public void setDespachoGenerado(Boolean despachoGenerado) {
+        this.despachoGenerado = despachoGenerado;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta{" +
+                "idVenta=" + idVenta +
+                ", direccionCompra='" + direccionCompra + '\'' +
+                ", valorCompra=" + valorCompra +
+                ", fechaCompra=" + fechaCompra +
+                ", despachoGenerado=" + despachoGenerado +
+                '}';
+    }
 }
