8155fc70d6ba593750a57e77621238520193ffc7
Ricardo Díaz <ric.diazs@duocuc.cl>
2026-05-30 06:17:10 -0400

    agrega getters y setters a clase Despacho

--- despachos/Springboot-API-REST-DESPACHO/src/main/java/com/citt/persistence/entity/Despacho.java
+++ despachos/Springboot-API-REST-DESPACHO/src/main/java/com/citt/persistence/entity/Despacho.java
@@ -31,4 +31,96 @@ public class Despacho {
     private String direccionCompra;
     private Long valorCompra;
     private boolean despachado = false;
+
+    public Despacho() {
+    }
+
+    public Despacho(Long idDespacho, LocalDate fechaDespacho, String patenteCamion, int intento, Long idCompra, String direccionCompra, Long valorCompra, boolean despachado) {
+        this.idDespacho = idDespacho;
+        this.fechaDespacho = fechaDespacho;
+        this.patenteCamion = patenteCamion;
+        this.intento = intento;
+        this.idCompra = idCompra;
+        this.direccionCompra = direccionCompra;
+        this.valorCompra = valorCompra;
+        this.despachado = despachado;
+    }
+
+    public Long getIdDespacho() {
+        return idDespacho;
+    }
+
+    public void setIdDespacho(Long idDespacho) {
+        this.idDespacho = idDespacho;
+    }
+
+    public LocalDate getFechaDespacho() {
+        return fechaDespacho;
+    }
+
+    public void setFechaDespacho(LocalDate fechaDespacho) {
+        this.fechaDespacho = fechaDespacho;
+    }
+
+    public String getPatenteCamion() {
+        return patenteCamion;
+    }
+
+    public void setPatenteCamion(String patenteCamion) {
+        this.patenteCamion = patenteCamion;
+    }
+
+    public int getIntento() {
+        return intento;
+    }
+
+    public void setIntento(int intento) {
+        this.intento = intento;
+    }
+
+    public Long getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Long idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getDireccionCompra() {
+        return direccionCompra;
+    }
+
+    public void setDireccionCompra(String direccionCompra) {
+        this.direccionCompra = direccionCompra;
+    }
+
+    public Long getValorCompra() {
+        return valorCompra;
+    }
+
+    public void setValorCompra(Long valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+
+    public boolean isDespachado() {
+        return despachado;
+    }
+
+    public void setDespachado(boolean despachado) {
+        this.despachado = despachado;
+    }
+
+    @Override
+    public String toString() {
+        return "Despacho{" +
+                "idDespacho=" + idDespacho +
+                ", fechaDespacho=" + fechaDespacho +
+                ", patenteCamion='" + patenteCamion + '\'' +
+                ", intento=" + intento +
+                ", idCompra=" + idCompra +
+                ", direccionCompra='" + direccionCompra + '\'' +
+                ", valorCompra=" + valorCompra +
+                ", despachado=" + despachado +
+                '}';
+    }
 }
